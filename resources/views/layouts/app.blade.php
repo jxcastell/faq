b5dbe501ec434e06e099c75092495e6273c18728
Jaime Castellanos <jc968@njit.edu>
2018-08-08 23:31:05 -0400

    Added Messages, error; messages and status.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,9 +89,14 @@
     </nav>
 
     <main class="py-4">
+
         <div class="col-12">
+            @include('flash.error')
+            @include('flash.messages')
+            @include('flash.status')
 
         </div>
+
         @yield('content')
     </main>
 </div>
